93ffbebb1c806b168a3fb12f834821665f9f38ab
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-04-18 11:46:13 +0300

    will this work?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,47 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    jest: true,
-    node: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'prettier/react',
-    'plugin:prettier/recommended'
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: '16.13.1',
-    },
-  },
-  plugins: [
-    'react',
-    'prettier'
-  ],
-  rules: {
-    'prettier/prettier': ['error'],
-  },
-}
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"prettier/react",
+		"plugin:prettier/recommended",
+	],
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: "module",
+	},
+	settings: {
+		react: {
+			pragma: "React",
+			version: "16.13.1",
+		},
+	},
+	plugins: ["react", "prettier"],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				tabWidth: 2,
+				semi: true,
+				singleQuote: true,
+				trailingComma: "all",
+				jsxBracketSameLine: true,
+				arrowParens: "always",
+				typeof: false,
+			},
+		],
+		// add some custom ESLint rules
+	},
+};
