1a5a5c5c7142b5e481dcb8c53ec5286329b08e61
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-04-18 13:26:29 +0300

    test husky fixing

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,16 +2,16 @@ const path = require('path');
 const config = require('./webpack.config.js');
 
 config.devServer = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers':
-      'X-Requested-With, content-type, Authorization',
-  },
-  historyApiFallback: true,
-  contentBase: path.join(__dirname, '../build'),
-  compress: true,
-  port: 8080,
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+		'Access-Control-Allow-Headers':
+			'X-Requested-With, content-type, Authorization',
+	},
+	historyApiFallback: true,
+	contentBase: path.join(__dirname, '../build'),
+	compress: true,
+	port: 8080,
 };
 
 config.devtool = 'inline-source-map';
